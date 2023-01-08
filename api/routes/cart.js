8bd49a33442305b83a75229a2ea639dc569872fd
ehoneahobed <ehoneahobed@hotmail.com>
2023-01-08 00:30:59 +0000

    Add cart and order routes

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,16 +1,68 @@
 const router = require('express').Router();
+const Cart = require('../models/Cart');
+const { verifyTokenAndAdmin, verifyTokenAndAuthorization } = require('./verifyToken');
 
-// routes for cart
 
-// get cart data
-router.get('/test', (req, res) => {
-    res.send("Test is successful");
+// routes for Carts
+
+// create Cart
+router.post('/', verifyTokenAndAdmin, async (req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
-// create cart
-router.post('/test', (req, res) => {
-    const {username, password} = req.body;
-    res.send(`Your username is ${username} and your password is ${password}`);
+// update Cart
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        }, {new: true});
+
+        res.status(200).json(updatedCart);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
+
+
+// delete Cart
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Cart was successfully deleted!");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
+// get user cart
+router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const cart = await Cart.findByOne({userId: req.params.userId});
+        res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// get all carts
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const carts = await Cart.find();
+        
+        res.status(200).json(carts);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 module.exports = router;
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,16 +1,68 @@
 const router = require('express').Router();
+const Order = require('../models/Order');
+const { verifyTokenAndAdmin, verifyTokenAndAuthorization } = require('./verifyToken');
 
-// routes for orders
 
-// get order data
-router.get('/test', (req, res) => {
-    res.send("Test is successful");
+// routes for Orders
+
+// create Order
+router.post('/', verifyTokenAndAdmin, async (req, res) => {
+    const newOrder = new Order(req.body);
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
-// create order
-router.post('/test', (req, res) => {
-    const {username, password} = req.body;
-    res.send(`Your username is ${username} and your password is ${password}`);
+// update Order
+router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        }, {new: true});
+
+        res.status(200).json(updatedOrder);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
+
+
+// delete Order
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Order was successfully deleted!");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
+// get user uart
+router.get('/find/:userId', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const order = await Order.findByOne({userId: req.params.userId});
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+// get all orders
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const orders = await Order.find();
+        
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 module.exports = router;
